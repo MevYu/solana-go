58e4793d04d15e51063c6f0e6b449aa5a60b6914
ciel <ciel@gmail.com>
2026-07-19 19:19:04 +0800

    update Simulation

--- rpc/types.go
+++ rpc/types.go
@@ -1,6 +1,11 @@
 package rpc
 
-import solana "github.com/MevYu/solana-go"
+import (
+	"encoding/json"
+	"math/big"
+
+	"github.com/MevYu/solana-go"
+)
 
 // GetSignatureStatusesResult is the decoded response of GetSignatureStatuses.
 type GetSignatureStatusesResult struct {
@@ -19,12 +24,41 @@ type SimulationReturnData struct {
 // remaining fields decode directly from the value object via their
 // json: tags.
 type SimulateResult struct {
-	Slot          uint64                `json:"-"`
-	Err           any                   `json:"err"`
-	Logs          []string              `json:"logs"`
-	UnitsConsumed *uint64               `json:"unitsConsumed"`
-	Accounts      []*solana.AccountInfo `json:"accounts"`
-	ReturnData    *SimulationReturnData `json:"returnData"`
+	Slot     uint64                `json:"-"`
+	Accounts []*solana.AccountInfo `json:"accounts"`
+	// Error if transaction failed, null if transaction succeeded.
+	// https://github.com/solana-labs/solana/blob/master/sdk/src/transaction.rs#L24
+	Err json.RawMessage `json:"err"`
+	// Fee this transaction was charged
+	Fee uint64 `json:"fee"`
+
+	// Array of *big.Int account balances from before the transaction was processed
+	PreBalances []*big.Int `json:"preBalances"`
+
+	// Array of *big.Int account balances after the transaction was processed
+	PostBalances []*big.Int `json:"postBalances"`
+
+	// List of inner instructions or omitted if inner instruction recording
+	// was not yet enabled during this transaction
+	InnerInstructions []solana.InnerInstruction `json:"innerInstructions"`
+
+	// List of token balances from before the transaction was processed
+	// or omitted if token balance recording was not yet enabled during this transaction
+	PreTokenBalances []solana.TokenBalance `json:"preTokenBalances"`
+
+	// List of token balances from after the transaction was processed
+	// or omitted if token balance recording was not yet enabled during this transaction
+	PostTokenBalances []solana.TokenBalance `json:"postTokenBalances"`
+
+	LoadedAccountsDataSize *uint32                `json:"loadedAccountsDataSize"`
+	LoadedAddresses        solana.LoadedAddresses `json:"loadedAddresses"`
+	// Array of string log messages or omitted if log message
+	// recording was not yet enabled during this transaction
+	Logs []string `json:"logs"`
+
+	ReplacementBlockhash *LastBlock            `json:"replacementBlockhash"`
+	ReturnData           *SimulationReturnData `json:"returnData"`
+	UnitsConsumed        *uint64               `json:"unitsConsumed"`
 }
 
 // BlockTransaction is a single entry in the transactions array of a
@@ -35,6 +69,13 @@ type BlockTransaction struct {
 	Transaction *solana.Transaction     `json:"transaction"`
 	Meta        *solana.TransactionMeta `json:"meta"`
 	Version     solana.MessageVersion   `json:"version"`
+}
+
+type LastBlock struct {
+	// a Hash as base-58 encoded string
+	Blockhash solana.Hash `json:"blockhash"`
+	//  last block height at which the blockhash will be valid
+	LastValidBlockHeight uint64 `json:"lastValidBlockHeight"`
 }
 
 // GetBlockResult is the decoded response of GetBlock.
